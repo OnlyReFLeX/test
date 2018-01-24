60787552a7e09390eeab864d94667601cd70c83e
OnlyReFLeX <reflexonly@gmail.com>
2018-01-24 19:16:15 +0300

    Lesson 2 fix1.1

--- lesson 2/hash.rb
+++ lesson 2/hash.rb
@@ -1,9 +1,7 @@
 vowels = {}
 vowels_letter = %w(a e o u i)
 ('a'..'z').each_with_index do |letter, x|
-  if vowels_letter.include?(letter)
-    vowels[letter] = x + 1
-  end
+  vowels[letter] = x + 1 if vowels_letter.include?(letter)
 end
 
 puts vowels
